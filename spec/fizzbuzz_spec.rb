8bdea16981c29b332dd3526dbffa5803ebae346e
James Ransome <jamesransome@msn.com>
2017-06-26 11:19:57 +0100

    added multiple of 5 test and production code

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -10,3 +10,10 @@ describe 'fizzbuzz' do
     end
   end
 end
+describe  'fizzbuzz' do
+  it 'returns "buzz" when passed a multiple of 5' do
+    (1..100).each do |n|
+      expect(fizzbuzz(n)).to eq 'buzz' if n % 5 == 0
+    end
+  end
+end
